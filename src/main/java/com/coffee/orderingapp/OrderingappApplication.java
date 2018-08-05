cc32980205438a3297cda5f1a8f5fe6ee4929ebc
Viyaan <viyaan.86@gmail.com>
2018-08-05 14:14:02 +0530

    spring boot

--- src/main/java/com/coffee/orderingapp/OrderingappApplication.java
+++ src/main/java/com/coffee/orderingapp/OrderingappApplication.java
@@ -1,12 +1,12 @@
-package com.coffee.CoffeeOrderingApp;
+package com.coffee.orderingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoffeeOrderingAppApplication {
+public class OrderingappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoffeeOrderingAppApplication.class, args);
+		SpringApplication.run(OrderingappApplication.class, args);
 	}
 }

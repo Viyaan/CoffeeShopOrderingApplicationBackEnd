2b446bcd4f5385cd408dff1e654608acbdaba619
Viyaan <viyaan.86@gmail.com>
2018-08-05 13:12:26 +0530

    base project

---
+++ src/main/java/com/coffee/CoffeeOrderingApp/CoffeeOrderingAppApplication.java
@@ -0,0 +1,12 @@
+package com.coffee.CoffeeOrderingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeOrderingAppApplication.class, args);
+	}
+}

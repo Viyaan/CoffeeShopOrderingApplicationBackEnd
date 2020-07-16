4ae6e28a990332f1173b7cb7a1d7f064c88290bc
Viyaan Jhiingade <30381782+Viyaan@users.noreply.github.com>
2020-07-16 22:00:19 +0530

    Update SwaggerConfig.java

--- src/main/java/com/coffee/orderingapp/config/SwaggerConfig.java
+++ src/main/java/com/coffee/orderingapp/config/SwaggerConfig.java
@@ -16,7 +16,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
+		
+		// code review
 
 	    @Bean
 	    public Docket apiDocket() {

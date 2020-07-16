c8dee690779b9059d42a098dd8349200761f5088
Viyaan Jhiingade <30381782+Viyaan@users.noreply.github.com>
2020-07-16 22:08:12 +0530

    deete

--- src/main/java/com/coffee/orderingapp/enums/OrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.coffee.orderingapp.enums;
-
-public enum OrderStatus {
-
-	ORDER_PLACED, IN_KITCHEN, OUT_FOR_DELIVERY, DELIVERED, CANCELLED
-}
--- src/main/java/com/coffee/orderingapp/enums/TokenStatus.java
+++
@@ -1,7 +0,0 @@
-package com.coffee.orderingapp.enums;
-
-public enum TokenStatus {
-
-	 ACTIVE, INACTIVE;
-
-}
